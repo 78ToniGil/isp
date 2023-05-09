f16872b097953dc412b59059113991c47535797b
Antonio R. Gil Ortega <60267621+78ToniGil@users.noreply.github.com>
2023-05-09 12:07:07 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Toni Gil - antonio.gil@hilltech.es
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "isp",
+    "name": "Telefonía",
     "summary": "Internet service providers",
     "version": "14.0.1.0.0",
     "category": "Services",
@@ -9,6 +9,7 @@
     "author": "Hilltech Digital",
     "license": "AGPL-3",
     "installable": True,
+    "application": True,
     "depends": [
         "base",
     ],
